bfbe1021f0f79147c132e53ab923c4537c5920fc
Gorka Olalde Mendia <gorka.olalde@gmail.com>
2016-05-27 01:27:35 +0200

    Refactored AdvancedTabController and separated the table creation to a new class(TopicTableManager)

--- src/main/java/controllers/AdvancedTabController.java
+++ src/main/java/controllers/AdvancedTabController.java
@@ -1,19 +1,10 @@
 package controllers;
 
-import com.jfoenix.controls.*;
+import com.jfoenix.controls.JFXButton;
 import controllers.dialogs.CreateTopicDialog;
-import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
-import javafx.collections.MapChangeListener;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.StackPane;
 import model.AppMain;
 import model.FXModel;
 import model.Topic;
@@ -35,10 +26,6 @@ public class AdvancedTabController {
 
     private FXModel model;
 
-    private final ObservableList topicsList = FXCollections.observableArrayList();
-    private ObservableList<Topic> selectedItems = FXCollections.observableArrayList();
-
-
     @FXML
     private JFXButton subscribeBtn;
 
@@ -48,131 +35,53 @@ public class AdvancedTabController {
     @FXML
     private TableView<Topic> topicTable;
 
-    @FXML
-    private TableColumn<Topic, String> topicNameColumn;
+    private TopicTableManager tableManager;
 
-    @FXML
-    private TableColumn<Topic, Boolean> subscribedColumn;
-
-    @FXML
-    private TableColumn<Topic, Long> timestampColumn;
-
-    @FXML
-    private TableColumn<Topic, Object> dataTypeColumn;
-
-    @FXML
-    private TableColumn<Topic, Object> valueColumn;
-
+    private ObservableList<Topic> selectedItems;
 
     /**
      * Sets the model to be used in the controller.
      * @param model The FXModel instance
      */
-    protected void setModel(FXModel model) {
+    private void setModel(FXModel model) {
         this.model = model;
     }
 
     /**
      * Initializes the controller with the default settings and assuming that a controller has been already set.
      */
-    protected void initController() {
-        initTableList();
-        initTable();
-        initColumns();
-        initCellRenderers();
+    private void initController() {
+        initTopicTableManager();
+        initButtons();
     }
 
     /**
      * Initializes the controller by setting it's model first.
      * @param model the FXModel instance.
      */
-    protected void initController(FXModel model) {
+    void initController(FXModel model) {
         setModel(model);
         initController();
     }
 
-    /**
-     * Initializes the table in which the topics are shown.
-     */
-    private void initTable() {
-        initTableList();
-        topicTable.itemsProperty().set(topicsList);
-        selectedItems = topicTable.getSelectionModel().getSelectedItems();
-        createSubscriptionButtonEnabler();
+
+    private void initTopicTableManager() {
+        tableManager = new TopicTableManager(topicTable);
+        tableManager.initialize(model.getTopics());
+        selectedItems = tableManager.selectedItemsProperty();
     }
 
-    /**
-     * Initializes the columns of the topic table and binds them with the properties inside the Topic class.
-     * @see Topic
-     */
-    private void initColumns() {
-        topicNameColumn.setCellValueFactory(new PropertyValueFactory<>("topicName"));
-        subscribedColumn.setCellValueFactory(new PropertyValueFactory<>("isSubscribed"));
-        timestampColumn.setCellValueFactory(new PropertyValueFactory<>("timestamp"));
-        dataTypeColumn.setCellValueFactory(new PropertyValueFactory<>("lastValue"));
-        valueColumn.setCellValueFactory(new PropertyValueFactory<>("lastValue"));
-    }
-
-    /**
-     * Initializes the cell renderers of the table.
-     */
-    private void initCellRenderers() {
-        dataTypeColumn.setCellFactory(column -> new TableCell<Topic, Object>() {
-            @Override
-            protected void updateItem(Object item, boolean empty) {
-                super.updateItem(item, empty);
-                if(item == null || empty) {
-                    setText("Unknown");
-                } else {
-                    if (item instanceof String) {
-                        setText("String");
-                    } else if (item instanceof Integer) {
-                        setText("Integer");
-                    } else if (item instanceof Boolean) {
-                        setText("Boolean");
-                    } else if (item instanceof Double) {
-                        setText("Double");
-                    } else {
-                        setText("Unknown class");
-                    }
-                }
-            }});
-    }
-
-    /**
-     * Create listener for enabling or disabling the subscribe and unsubscribe buttons depending on the selection.
-     * Depending of the current subscribe state of the selected topics in the table, enables or disables the buttons.
-     */
-    private void createSubscriptionButtonEnabler() {
-        selectedItems.addListener((ListChangeListener<Topic>) e -> {
-            int subscribedItems = 0;
-            int unSubscribedItems = 0;
-            for(Topic item : selectedItems) {
-                if (item.isSubscribed()) {
-                    subscribedItems++;
-                } else {
-                    unSubscribedItems++;
-                }
-            }
-            unSubscribeBtn.setDisable(subscribedItems == 0);
-            subscribeBtn.setDisable(unSubscribedItems == 0);
-
+    private void initButtons() {
+        subscribeBtn.setDisable(true);
+        unSubscribeBtn.setDisable(true);
+        tableManager.hasItemsToSubscribeProperty().addListener((o, oldValue, newValue) -> {
+            subscribeBtn.setDisable(!newValue);
+        });
+        tableManager.hasItemsToUnsubscribeProperty().addListener((o, oldValue, newValue) -> {
+            unSubscribeBtn.setDisable(!newValue);
         });
     }
 
-    /**
-     * Initializes tha list where the topics of the table will be stored and monitors the list in the model for changes.
-     */
-    private void initTableList() {
-        topicsList.addAll(model.getTopics().values());
-        model.getTopics().addListener((MapChangeListener<String, Topic>) change -> {
-            if (change.wasAdded()) {
-                topicsList.add(change.getValueAdded());
-            } else if (change.wasRemoved()) {
-                topicsList.remove(change.getValueRemoved());
-            }
-        });
-    }
 
     /**
      * Subscribe to selection button action handler.
@@ -201,9 +110,13 @@ public class AdvancedTabController {
      * @throws IOException
      */
     @FXML
-    private void createNewTopic() throws IOException {
+    private void createNewTopic() {
         CreateTopicDialog dialog = new CreateTopicDialog(AppMain.getStage());
-        dialog.showAndWait();
+        dialog.showAndWait().ifPresent(response -> {
+            if(response !=null) {
+                model.publish(response);
+            }
+        });
 
     }
 
---
+++ src/main/java/controllers/TopicTableManager.java
@@ -0,0 +1,218 @@
+package controllers;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.collections.*;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Topic;
+
+import java.text.DateFormat;
+
+
+/**
+ * Created by Gorka Olalde on 26/5/16.
+ */
+class TopicTableManager {
+
+    private final TableView topicTable;
+
+    private ObservableList<Topic> topicList;
+
+    private ObservableList<Topic> selectedItems;
+
+    private TableColumn<Topic, String> topicNameColumn;
+
+    private TableColumn<Topic, Boolean> subscribedColumn;
+
+    private TableColumn<Topic, Long> timestampColumn;
+
+    private TableColumn<Topic, Object> dataTypeColumn;
+
+    private TableColumn<Topic, Object> valueColumn;
+
+    private final BooleanProperty hasItemsToSubscribe = new SimpleBooleanProperty();
+
+    private final BooleanProperty hasItemsToUnsubscribe = new SimpleBooleanProperty();
+
+
+    public TopicTableManager(TableView<Topic> topicTable) {
+        this.topicTable = topicTable;
+    }
+
+    public void initialize(ObservableMap<String, Topic> topicMap) {
+        setTableItemList(topicMap);
+        initialize();
+    }
+
+    public void initialize(ObservableList<Topic> topicList) {
+        setTableItemList(topicList);
+        initialize();
+    }
+
+    BooleanProperty hasItemsToSubscribeProperty() { return hasItemsToSubscribe; }
+
+    BooleanProperty hasItemsToUnsubscribeProperty() { return hasItemsToUnsubscribe; }
+
+    ObservableList<Topic> selectedItemsProperty() { return selectedItems; }
+
+    private void initialize() {
+        initColumns();
+        initColumnDataSources();
+        initCellRenderers();
+        initTableLists();
+        initSelectedItemsSubscriptionMonitor();
+    }
+
+    /**
+     * Initializes the columns of the topic table and binds them with the properties inside the Topic class.
+     * @see Topic
+     */
+    private void initColumns() {
+        topicNameColumn = new TableColumn<>("Topic Name");
+        topicNameColumn.setPrefWidth(300);
+
+        subscribedColumn = new TableColumn<>("Is subscribed?");
+        subscribedColumn.setPrefWidth(180);
+
+        timestampColumn = new TableColumn<>("Timestamp");
+        timestampColumn.setPrefWidth(180);
+
+        dataTypeColumn = new TableColumn<>("Data type");
+        dataTypeColumn.setPrefWidth(150);
+
+        valueColumn = new TableColumn<>("Last value");
+        valueColumn.setPrefWidth(200);
+
+        topicTable.getColumns().addAll(
+                topicNameColumn,
+                subscribedColumn,
+                timestampColumn,
+                dataTypeColumn,
+                valueColumn
+        );
+    }
+
+    private void initColumnDataSources() {
+        topicNameColumn.setCellValueFactory(new PropertyValueFactory<>("topicName"));
+        subscribedColumn.setCellValueFactory(new PropertyValueFactory<>("isSubscribed"));
+        timestampColumn.setCellValueFactory(new PropertyValueFactory<>("lastValueTimestamp"));
+        dataTypeColumn.setCellValueFactory(new PropertyValueFactory<>("lastValue"));
+        valueColumn.setCellValueFactory(new PropertyValueFactory<>("lastValue"));
+    }
+
+    /**
+     * Initializes the cell renderers of the table.
+     */
+    private void initCellRenderers() {
+        dataTypeColumn.setCellFactory(column -> new TableCell<Topic, Object>() {
+            @Override
+            protected void updateItem(Object item, boolean empty) {
+                super.updateItem(item, empty);
+                if(item == null || empty) {
+                    setText("");
+                } else {
+                    if (item instanceof String) {
+                        setText("String");
+                    } else if (item instanceof Integer) {
+                        setText("Integer");
+                    } else if (item instanceof Boolean) {
+                        setText("Boolean");
+                    } else if (item instanceof Double) {
+                        setText("Double");
+                    } else {
+                        setText("Unknown class");
+                    }
+                }
+            }});
+        subscribedColumn.setCellFactory(column -> new TableCell<Topic, Boolean>() {
+            @Override
+            protected void updateItem(Boolean item, boolean empty) {
+                super.updateItem(item, empty);
+                if (item == null || empty) {
+                    setText("");
+                } else {
+                    if(item) {
+                        setText("Subscribed");
+                    } else {
+                        setText("Unsubscribed");
+                    }
+                }
+            }
+        });
+        timestampColumn.setCellFactory(column -> new TableCell<Topic, Long>() {
+            @Override
+            protected void updateItem(Long item, boolean empty) {
+                super.updateItem(item, empty);
+                if(item == null || empty) {
+                    setText("");
+                } else {
+                    setText(DateFormat.getTimeInstance(DateFormat.FULL).format(item));
+                }
+            }
+        });
+    }
+
+    private void initTableLists() {
+        topicTable.setItems(topicList);
+        selectedItems = topicTable.getSelectionModel().getSelectedItems();
+    }
+
+
+    /**
+     * Set table item list based on an external ObservableMap.
+     * @param topicMap The external topic ObservableMap.
+     */
+    private void setTableItemList(ObservableMap<String, Topic> topicMap) {
+        if (topicList == null) {
+            topicList = FXCollections.observableArrayList();
+        } else {
+            topicList.clear();
+        }
+        topicList.addAll(topicMap.values());
+        topicMap.addListener((MapChangeListener<String, Topic>) change -> {
+            if (change.wasAdded()) {
+                topicList.add(change.getValueAdded());
+            } else if (change.wasRemoved()) {
+                topicList.remove(change.getValueRemoved());
+            }
+        });
+    }
+
+    /**
+     * Set table item list to be based on an external ObservableList.
+     * @param topicList The external ObservableList.
+     */
+    private void setTableItemList(ObservableList<Topic> topicList) {
+        if (topicList == null) {
+            this.topicList = topicList;
+        } else {
+            this.topicList.clear();
+            this.topicList.addAll(topicList);
+        }
+    }
+
+    /**
+     * Create listener to check if any of the selected items is suitable for unsubscribing from it or subscribing to it.
+     * Depending of the current subscribe state of the selected topics in the table, sets their respective properties.
+     */
+    private void initSelectedItemsSubscriptionMonitor() {
+       selectedItems.addListener((ListChangeListener<Topic>) e -> {
+            int subscribedItems = 0;
+            int unSubscribedItems = 0;
+            for(Topic item : selectedItems) {
+                if (item.isSubscribed()) {
+                    subscribedItems++;
+                } else {
+                    unSubscribedItems++;
+                }
+            }
+            hasItemsToUnsubscribe.set(subscribedItems != 0);
+            hasItemsToSubscribe.set(unSubscribedItems != 0);
+        });
+    }
+
+
+}
