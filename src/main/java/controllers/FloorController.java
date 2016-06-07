05da9134eccfa65e6313447954d43027adafc5ee
Gorka Olalde Mendia <gorka.olalde@gmail.com>
2016-06-07 13:37:28 +0200

    Implemented floor plan image brightness changing when the brightness slider value is changed or an external brightness value is received from the middleware

--- src/main/java/controllers/FloorController.java
+++ src/main/java/controllers/FloorController.java
@@ -4,8 +4,11 @@ import com.jfoenix.controls.JFXSlider;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXToggleButton;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import model.AppMain;
 import model.FXModel;
@@ -22,6 +25,9 @@ public class FloorController {
 
     private FXModel model;
     private AnchorPane rootNode;
+    private ColorAdjust colorAdjust = new ColorAdjust();
+
+    private ImageView floorPlan;
 
     private ObservableList<Node> deviceControls;
 
@@ -46,6 +52,10 @@ public class FloorController {
                 configureSliderDevice((JFXSlider) node);
             } else if (node instanceof JFXTextField) {
                 configureTextDevice((JFXTextField) node);
+            } else if (node instanceof ImageView) {
+                if (node.getId().equals("floorPlan")) {
+                    floorPlan = (ImageView)node;
+                }
             }
         }
     }
@@ -81,6 +91,9 @@ public class FloorController {
             deviceTopic.lastValueProperty().addListener((o, oldVal, newVal) -> {
                 slider.valueProperty().set((Double) newVal);
             });
+            if(deviceTopic.getTopicName().contains("lightIntensity")) {
+                initBrightnessDevice(deviceTopic);
+            }
         }
     }
 
@@ -99,5 +112,17 @@ public class FloorController {
         }
     }
 
+    private void initBrightnessDevice(Topic lightTopic) {
+        final int maxSliderRange = 100;
+        final int minSliderOutput = 50;
+        final int minBrightnessRange = -1;
+        floorPlan.effectProperty().set(colorAdjust);
+        lightTopic.lastValueProperty().addListener((o, oldVal, newVal) -> {
+            Double input = (Double)newVal + minSliderOutput;
+            Double mapping = input / maxSliderRange + minBrightnessRange;
+            System.out.println("Mapping: " + mapping);
+            colorAdjust.setBrightness(mapping);
+        });
+    }
 
 }
---
+++ src/main/java/controllers/dialogs/ConfigDialog.java
@@ -0,0 +1,7 @@
+package controllers.dialogs;
+
+/**
+ * Created by Gorka Olalde on 7/6/16.
+ */
+public class ConfigDialog {
+}
